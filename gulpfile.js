1cf0c20714836a8c6e53f2a2fc6cd45f1f02ca67
Mark Holden <markholden1989@googlemail.com>
2020-02-17 15:08:25 +0000

    Minor updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ const { watch, src, dest } = require("gulp");
 const babel = require("gulp-babel");
 const postcss = require("gulp-postcss");
 const rollup = require("gulp-better-rollup");
+const cleanup = require("rollup-plugin-cleanup");
 const del = require("del");
+const prettier = require("gulp-prettier");
 
 const src_folder = "src/";
 const prod_folder = "prod/";
@@ -11,20 +13,37 @@ const assets_src_folder = `${src_folder}assets/`;
 const assets_prod_folder = `${prod_folder}assets/`;
 
 function javascript() {
-    return src(`${src_folder}*.js`)
-        .pipe(rollup({}, { format: "iife" }))
+    return src(`${src_folder}**/*.js`)
+        .pipe(
+            rollup(
+                {
+                    plugins: [
+                        cleanup({
+                            maxEmptyLines: 1,
+                            comments: "all",
+                            lineEndings: "unix",
+                            sourcemap: false,
+                        }),
+                    ],
+                },
+                {
+                    format: "iife",
+                }
+            )
+        )
         .pipe(babel())
+        .pipe(prettier())
         .pipe(dest(prod_folder));
 }
 
 function css() {
-    return src(`${src_folder}*.css`)
+    return src(`${src_folder}**/*.css`)
         .pipe(postcss())
         .pipe(dest(prod_folder));
 }
 
 function html() {
-    return src(`${src_folder}*.html`).pipe(dest(prod_folder));
+    return src(`${src_folder}**/*.html`).pipe(dest(prod_folder));
 }
 
 function assets() {
@@ -32,9 +51,9 @@ function assets() {
 }
 
 exports.watch = function() {
-    watch(`${src_folder}*.js`, { ignoreInitial: false }, javascript);
-    watch(`${src_folder}*.css`, { ignoreInitial: false }, css);
-    watch(`${src_folder}*.html`, { ignoreInitial: false }, html);
+    watch(`${src_folder}**/*.js`, { ignoreInitial: false }, javascript);
+    watch(`${src_folder}**/*.css`, { ignoreInitial: false }, css);
+    watch(`${src_folder}**/*.html`, { ignoreInitial: false }, html);
     watch(`${assets_src_folder}**/*`, { ignoreInitial: false }, assets);
 };
 
@@ -45,4 +64,10 @@ exports.assets = assets;
 
 exports.clear = function() {
     return del([`${src_folder}/**`, `${prod_folder}/**`]);
+};
+exports.clearSrc = function() {
+    return del([`${src_folder}/**`]);
+};
+exports.clearProd = function() {
+    return del([`${prod_folder}/**`]);
 };
--- modules/buildElement.js
+++ modules/buildElement.js
@@ -1,19 +1,22 @@
 import getNestedProperty from "./getNestedProperty.js";
 import addClassToggleToElemets from "./addClassToggleToElements.js";
 
-addClassToggleToElemets();
-
 /**
  *  Improved version of document.createElement, taking in type and object with options
  */
 export default function buildElement(elementType, options) {
+    if (!Element.prototype.toggleClass) {
+        addClassToggleToElemets();
+    }
+
     let newElement = document.createElement(elementType);
 
-    if (typeof options !== "object" || options instanceof Array)
-        return undefined;
+    // if (typeof options !== "object" || options instanceof Array)
+    //     return undefined;
 
     let classes = getNestedProperty(options, "classes", false);
     let attributes = getNestedProperty(options, "attributes", false);
+    let id = getNestedProperty(options, "id", false);
 
     if (classes.length) {
         classes.forEach((c) => newElement.toggleClass(c));
@@ -21,6 +24,10 @@ export default function buildElement(elementType, options) {
 
     if (attributes.length) {
         attributes.forEach((a) => newElement.toggleClass(a));
+    }
+
+    if (id) {
+        newElement.id = id;
     }
 
     return newElement;
